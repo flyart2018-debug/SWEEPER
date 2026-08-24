0bbb349cdcfd8593694bec60137be1ee02bd3771
flyart2018-debug <flyart2018@gmail.com>
2026-08-24 18:20:44 +0900

    CharacterData.js を作成

---
+++ src/characters/CharacterData.js
@@ -0,0 +1,128 @@
+export const CHARACTER_DATA = {
+
+    REN: {
+
+        id: "REN",
+
+        name: "レン・クロス",
+
+        shortName: "REN",
+
+        type: "バランス型",
+
+        maxHp: 100,
+
+        color: "#2d7cff",
+
+        weapon: "ブレード",
+
+        attackDamage: 30,
+
+        attackRange: 1,
+
+        ability: "オーバードライブ",
+
+        uniqueChip: "OVER_BLADE",
+
+        description:
+            "攻撃・防御・移動を平均的に扱えるオールラウンダー。"
+
+    },
+
+
+    KAI: {
+
+        id: "KAI",
+
+        name: "カイ・ヴェルド",
+
+        shortName: "KAI",
+
+        type: "スピード型",
+
+        maxHp: 100,
+
+        color: "#7dff45",
+
+        weapon: "ナイフ",
+
+        attackDamage: 20,
+
+        attackRange: 1,
+
+        ability: "ステップ",
+
+        uniqueChip: "ACCEL_STEP",
+
+        description:
+            "高い機動力で攻撃と離脱を繰り返す高速型。"
+
+    },
+
+
+    /*
+     * Future characters
+     *
+     * α版では選択不可。
+     */
+
+    GARDO: {
+
+        id: "GARDO",
+
+        name: "ガルド・ブレイク",
+
+        shortName: "GARDO",
+
+        type: "パワー型",
+
+        maxHp: 100,
+
+        color: "#ff4b45",
+
+        weapon: "ハンマー",
+
+        attackDamage: 50,
+
+        attackRange: 1,
+
+        ability: "チャージ",
+
+        uniqueChip: "MEGA_HAMMER",
+
+        description:
+            "高火力のパワー型。"
+
+    },
+
+
+    SHION: {
+
+        id: "SHION",
+
+        name: "シオン・レイヴ",
+
+        shortName: "SHION",
+
+        type: "テクニック型",
+
+        maxHp: 100,
+
+        color: "#b76cff",
+
+        weapon: "ショット",
+
+        attackDamage: 20,
+
+        attackRange: 3,
+
+        ability: "スキャン",
+
+        uniqueChip: "TRAP",
+
+        description:
+            "遠距離攻撃と特殊効果を扱うテクニック型。"
+
+    }
+
+};
